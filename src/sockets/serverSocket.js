d4437b60225b9b8a85388801382305906dd33f8c
Brandom Galder Hernandez Franco <galderhernandez571@gmail.com>
2026-03-02 14:19:57 -0600

    Conexión a través de rooms con frontend

--- src/sockets/serverSocket.js
+++ src/sockets/serverSocket.js
@@ -1,12 +1,19 @@
 export default function serverSocket(io) {
 
   io.on('connect', (socket) => {
-    console.log('a user connected to partida socket', socket.id);
+    // console.log('a user connected to partida socket', socket.id);
   
-    socket.on('movimiento', (data) => {
-      console.log('Movimiento recibido:', data);
+    socket.on('unirse_sala', (salaId) => {
+      socket.leaveAll();
 
-      socket.broadcast.emit('movimiento', data);
+      // console.log(`Usuario ${socket.id} se unió a la sala ${salaId}`);
+      socket.join(salaId);
+    });
+
+    socket.on('movimiento', (data) => {
+      // console.log('Movimiento recibido:', data);
+
+      socket.to(data.sala).emit('movimiento', data);
     });
   });
 
